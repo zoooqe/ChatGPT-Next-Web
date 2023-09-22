0ac15e9e793176519595d51fcc273dcdd1b3eaa7
zoooqe <129477897+zoooqe@users.noreply.github.com>
2023-09-22 13:33:51 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -88,18 +88,18 @@ export const DEFAULT_MODELS = [
     name: "gpt-4-0613",
     available: true,
   },
-  {
-    name: "gpt-4-32k",
-    available: true,
-  },
-  {
-    name: "gpt-4-32k-0314",
-    available: true,
-  },
-  {
-    name: "gpt-4-32k-0613",
-    available: true,
-  },
+  // {
+  //   name: "gpt-4-32k",
+  //   available: true,
+  // },
+  // {
+  //   name: "gpt-4-32k-0314",
+  //   available: true,
+  // },
+  // {
+  //   name: "gpt-4-32k-0613",
+  //   available: true,
+  // },
   {
     name: "gpt-3.5-turbo",
     available: true,
@@ -112,14 +112,14 @@ export const DEFAULT_MODELS = [
     name: "gpt-3.5-turbo-0613",
     available: true,
   },
-  {
-    name: "gpt-3.5-turbo-16k",
-    available: true,
-  },
-  {
-    name: "gpt-3.5-turbo-16k-0613",
-    available: true,
-  },
+  // {
+  //   name: "gpt-3.5-turbo-16k",
+  //   available: true,
+  // },
+  // {
+  //   name: "gpt-3.5-turbo-16k-0613",
+  //   available: true,
+  // },
 ] as const;
 
 export const CHAT_PAGE_SIZE = 15;
